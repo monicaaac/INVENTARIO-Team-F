d4a0ba61a1374ef8cb4fd74394f00f0f01b84c11
monicaaac <maguileraa@uninorte.edu.co>
2020-12-20 17:27:51 -0500

    update

--- index.py
+++ index.py
@@ -3,12 +3,16 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def login():
-    return render_template('login.html')
+@app.route('/')
+def Login():
+    return render_template('Login.html')
+
+@app.route('/recuperar_psswd')
+def recuperar_psswd():
+    return render_template('recuperar_psswd.html')
 
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     app.run(debug=True)
-
 
