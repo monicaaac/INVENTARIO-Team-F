a14a65f51f1c2213456f861f2b8a1b05c34a55c5
monicaaac <maguileraa@uninorte.edu.co>
2020-12-20 19:02:06 -0500

    update 7pm

--- static/js/login.js
+++ static/js/login.js
@@ -1,7 +1,7 @@
 function validate(){
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
-    if ( username == "Maguileraa" && password == "password1"){
+    if ( username == "Monica" && password == "password1"){
     alert ("Ingreso Exitoso");
          }
          else{
@@ -19,3 +19,48 @@ function request_email (valor){
 	}
 	else alert('email valido, el correo ha sido enviado!!');
   }
+
+
+  //validar formulario
+
+  function validar_formulario(){
+    var usuario = document.getElementById("username");
+    var correo = document.getElementById("email");
+    var clave = document.getElementById("password");
+        
+    var usuario_len = usuario.value.length;
+    if(usuario_len == 0 || usuario_len < 8)
+    {
+        alert("Debes ingresar un usuario con mínimo 8 caracteres");
+        // passid.focus();
+        return false; //Para la parte dos, que los datos se conserven
+    }
+
+    var formatoCorreo = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if(!correo.value.match(formatoCorreo))
+    {
+        alert("Debes ingresar un correo electronico valido!");
+        // correo.focus();
+        return false; //Para la parte dos, que los datos se conserven
+    }
+    
+    var passid_len = clave.value.length;
+
+    if (passid_len == 0 || passid_len < 8)
+    {
+        alert("Debes ingresar una clave con mas de 8 caracteres");
+        // passid.focus();
+        return false;
+    }
+
+}
+
+function mostrarPassword() {
+    var obj = document.getElementById('password');
+    obj.type = "text";
+}
+
+function ocultarPassword() {
+    var obj = document.getElementById('password');
+    obj.type = "password";
+}
